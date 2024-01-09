8941de22fa04d40636f13d85c8f05803c83262ff
arkaprav <chakraarkaprava@gmail.com>
2024-01-09 19:20:16 +0530

    changed site name in server.js

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 server.listen(port,() => {
-    console.log(`PicsaGramBackend started on PORT NO.: ${port}`);
+    console.log(`Ecommerce Backend API started on PORT NO.: ${port}`);
 });
